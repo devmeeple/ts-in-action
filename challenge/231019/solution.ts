3be32f709f1e4ad7c9b72a38c1fcbbecf7a6dfcd
미플 <devmeeple@gmail.com>
2023-10-20 00:00:02 +0900

    feat: 231019 문제 구현

---
+++ challenge/231019/solution.ts
@@ -0,0 +1,18 @@
+export const solution = {
+    emptyAverage(numbers: number[]) {
+        let sum = 0;
+        numbers.forEach(num => {
+            sum += num;
+        });
+        return sum / numbers.length;
+    },
+    reverseArray(num_list: number[]) {
+        return num_list.reverse();
+    },
+    sliceArray(numbers: number[], num1: number, num2: number) {
+        return numbers.slice(num1, num2 + 1);
+    },
+    countElement(array: number[], n: number) {
+        return array.filter((value) => value === n).length;
+    }
+};
