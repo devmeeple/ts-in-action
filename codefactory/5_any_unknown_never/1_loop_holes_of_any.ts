fff1ad529a0f5015ab19adc0101a78040c3fba1b
미플 <devmeeple@gmail.com>
2023-10-29 22:48:29 +0900

    [#24] feat: 섹션5. Any / Unknown / Never
    - Loopholes of Any (Any의 문제점)
    - Unknown Type
    - Never Type

---
+++ codefactory/5_any_unknown_never/1_loop_holes_of_any.ts
@@ -0,0 +1,32 @@
+/**
+ * Loopholes of Any
+ */
+let number: number;
+number = 10;
+
+// number.toUpperCase();
+
+// (number as any).toUpperCase();
+
+const multiplyTwo = (x: number, y: number) => {
+    return x * y;
+};
+
+let args1: any = '미플';
+let args2: any = true;
+
+multiplyTwo(args1, args2);
+// multiplyTwo('미플', true);
+
+let iu: any = { name: '아이유', age: 30, };
+iu;
+
+const callbackRunner = (x: number, y: number, callback: any) => {
+    return callback(x);
+};
+
+const callback = (x: number, y: number) => {
+    return x * y;
+};
+
+console.log(callbackRunner(5, 4, callback));
