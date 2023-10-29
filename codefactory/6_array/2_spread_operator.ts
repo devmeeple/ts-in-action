0860d3659be1db585519d506558fadbf6af21ac3
미플 <devmeeple@gmail.com>
2023-10-30 01:00:18 +0900

    [#26] feat: 섹션 6. Array (배열)
    - Javascript Array의 문제점
    - Spread Operator
    - Multi Dimension Array

---
+++ codefactory/6_array/2_spread_operator.ts
@@ -0,0 +1,14 @@
+/**
+ * Spread Operator
+ */
+const onlyStrings = ['1', '2', '3'];
+const onlyNumbers = [1, 2, 3, 4];
+
+const arr1 = [
+    ...onlyStrings,
+];
+
+const arr2 = [
+    ...onlyNumbers,
+    ...onlyStrings,
+];
