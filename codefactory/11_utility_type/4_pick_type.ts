c18af21cf9ceadd45d3d6364e149f4cfe77d2e8b
미플 <devmeeple@gmail.com>
2023-10-31 02:38:39 +0900

    [#38] feat: 섹션 11. Utility Types (유틸리티 타입)
    - Partial Type
    - Required Type
    - Readonly Type
    - Pick Type
    - Omit Type
    - Exclude Type
    - Extract Type
    - NonNullable Type
    - Parameters Type
    - ConstructorParameters Type
    - Return Type
    - Template Literal Types

---
+++ codefactory/11_utility_type/4_pick_type.ts
@@ -0,0 +1,20 @@
+/**
+ * Pick Type
+ */
+interface Post {
+    title: string;
+    content: string;
+    createdAt: Date;
+}
+
+function createPost(post: Pick<Post, 'title' | 'content'>): Post {
+    return{
+        ...post,
+        createdAt: new Date(),
+    };
+}
+
+createPost({
+    title: '좌측담장, 좌측담장',
+    content: '으로 들어가시면 됩니다.',
+});
---
+++ codefactory/11_utility_type/9_parameter_type.ts
@@ -0,0 +1,9 @@
+/**
+ * Parameter Type
+ */
+function sampleFunction(x: number, y: string, z: boolean) {
+    
+}
+
+type Params = Parameters<typeof sampleFunction>;
+type Param2 = Parameters<(one: number) => void>;
