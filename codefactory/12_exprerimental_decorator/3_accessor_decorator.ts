24143739dec951757e3b7cf5f2dbd9e405ce4eb9
미플 <devmeeple@gmail.com>
2023-10-31 14:20:12 +0900

    [#40] feat: 섹션 12. Experimental Decorators (실험적 데코레이터)
    - Class Decorator (클래스 데코레이터)
    - Method Decorator (메서드 데코레이터)
    - Accessor Decorator (액세서 데코레이터)
    - Property Decorator (프로퍼티 데코레이터)
    - Parameter Decorator (파라미터 데코레이터)

---
+++ codefactory/12_exprerimental_decorator/3_accessor_decorator.ts
@@ -0,0 +1,32 @@
+/**
+ * Accessor Decorator
+ */
+class Rectangle {
+    #height: number;
+    #width: number;
+
+    constructor(height: number, width: number) {
+        this.#height = height;
+        this.#width = width;
+    }
+
+    @Configurable(false)
+    get height(): number {
+        return this.#height;
+    }
+
+    @Configurable(true)
+    get width(): number {
+        return this.#width;
+    }
+}
+
+function Configurable(configurable: boolean) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        descriptor.configurable = configurable;
+    };
+}
+
+const rectangle = new Rectangle(100, 200);
+
+console.log(Object.getOwnPropertyDescriptors(Rectangle.prototype));
