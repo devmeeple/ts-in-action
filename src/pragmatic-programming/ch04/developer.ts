e792896ec8d0e854b036ec660b3a086aac343507
미플 <devmeeple@gmail.com>
2024-08-09 18:34:18 +0900

    feat(pragmatic): 4.1.1 단일 책임 원칙(SRP: Single Responsibility Principle) 예제
    - 4.1.1 개발자를 표현하는 클래스

---
+++ src/pragmatic-programming/ch04/developer.ts
@@ -0,0 +1,19 @@
+/**
+ * 4.1 개발자를 표현하는 클래스
+ *
+ * 프런트엔드 코드를 만든다.
+ * 프런트엔드 서비스를 배포한다.
+ * 백엔드 코드를 만든다.
+ * 백엔드 서비스를 배포한다.
+ *
+ * 단일 책임 원칙(Single Responsibility Principle)이란 무엇일까.
+ */
+class Developer {
+  createFrontendCode() {}
+
+  publishFronted() {}
+
+  createBackendCode() {}
+
+  serveBackend() {}
+}
