4ebda13c7d594ca4db8744a23cdba76f8c4b8b32
미플 <devmeeple@gmail.com>
2024-08-24 23:31:13 +0900

    test(readable-code): Refactor 정적 팩토리 메서드를 사용한 객체 생성

--- test/readable-code/money.spec.ts
+++ test/readable-code/money.spec.ts
@@ -1,21 +1,25 @@
 class Money {
-  private value: number;
+  private readonly value: number;
 
-  constructor(value: number) {
+  private constructor(value: number) {
+    this.value = value;
+  }
+
+  static of(value: number) {
     if (value < 0) {
       throw new Error('돈은 0원 이상이어야 합니다.');
     }
-    this.value = value;
+    return new Money(value);
   }
 }
 
 describe('MoneyTest', () => {
-  it('[생성자] 0 이하를 입력하면 객체를 생성할 수 없다. [실패]', () => {
+  it('[정적 팩토리 메서드] 0 이하를 입력하면 객체를 생성할 수 없다. [실패]', () => {
     // given
 
     // when
 
     // then
-    expect(() => new Money(-1000)).toThrow(Error);
+    expect(() => Money.of(-1000)).toThrow(Error);
   });
 });
